ea3e13661ce49f68e12e3121e9aa7317fd240537
JamesCMarch <a1769591@student.adelaide.edu.au>
2021-03-05 13:36:39 +1030

    Update thing.rb

--- thing.rb
+++ thing.rb
@@ -1,1 +1,18 @@
 puts "hello world"
+i = 0
+x = 10
+for i in 1..x do
+	puts i
+	puts "hi #{i} james"
+end
+while x < 20 do
+	puts x
+	x= x+1
+end
+
+y = 2
+if y < 1
+	puts "nope"
+else
+	puts "yes"
+end
